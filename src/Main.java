5b53cb635df6a0a9560e9d6a812b06c234ccaa15
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-12 18:33:58 -0700

    First Methods!

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        //System.out.println("Your final score was ");
+    }
+
+    //first method... so it's like a function in powershell... neat I know this.
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+
+            return finalScore;
+        }
+
+        return -1;
+
+    }
+}
